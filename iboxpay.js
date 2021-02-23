986a4f08ab19a1f5b5dda64ce6f8b62c680e4f57
Nell3582 <qianye1203@outlook.com>
2021-02-23 11:30:29 +0800

    add ranlv.js

--- iboxpay.js
+++ iboxpay.js
@@ -105,7 +105,7 @@ http-response https:\/\/veishop\.iboxpay\.com\/nf_gateway\/nf-user-auth-web\/ign
 */
 const $ = Env("笑谱");
 $.idx = ($.idx = ($.getval('iboxpaySuffix') || '1') - 1) > 0 ? ($.idx + 1 + '') : ''; // 账号扩展字符
-const notify = $.isNode() ? require("./sendNotify") : ``;
+// const notify = $.isNode() ? require("./sendNotify") : ``;
 const COOKIE = $.isNode() ? require("./iboxpayCOOKIE") : ``;
 const logs = 0; // 0为关闭日志，1为开启
 const notifyttt = 1 // 0为关闭外部推送，1为12 23 点外部推送
---
+++ rvtest.js
@@ -0,0 +1,10 @@
+obj = {
+    'Host': 'ranlv.lvfacn.com',
+    'accept': '*/*',
+    'user-agent': 'ran lu shi pin/1.0.49 (iPhone; iOS 14.3; Scale/2.00)',
+    'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
+    'Cookie': 'acw_tc=df6f181516140471385893404ebc5dbf8a720d33bf155c2a1c9ffdbf9b'
+}
+
+a = JSON.stringify(obj)
+console.log(a)
