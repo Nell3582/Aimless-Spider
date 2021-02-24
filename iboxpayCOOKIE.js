81f8cd36511141a999eb6739b0b0fa7a452548ce
Nell3582 <qianye1203@outlook.com>
2021-02-24 23:45:29 +0800

    update iboxpay

--- iboxpayCOOKIE.js
+++ iboxpayCOOKIE.js
@@ -3,7 +3,7 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
 let refreshtokenVal= `98485ef5a0984bb99fb8527c30076069
-d04d5f1c53de4baa98b53dc27850f1f6`
+b765d926297a4fc0b18d1df8ec88c8bb`
 
 
 let iboxpaycookie = {
--- iboxpayCOOKIE2.js
+++ iboxpayCOOKIE2.js
@@ -2,9 +2,9 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let refreshtokenVal= `
-390a5549d9ab4c4085de1f277e72afb1
-a2bcb607757540358175bb6c98d74a1a`
+let refreshtokenVal= `390a5549d9ab4c4085de1f277e72afb1
+a2bcb607757540358175bb6c98d74a1a
+cbd0ad489b1e435782afbbbcaa556b6c`
 
 
 let iboxpaycookie = {
