16649bf6996b647f0a200209622d7982546080ac
Nell3582 <qianye1203@outlook.com>
2021-02-25 15:25:53 +0800

    reload hotsoon cookies

--- json转义.js
+++ json转义.js
@@ -1,16 +1,16 @@
 var headers = {
     'Host': 'ib.snssdk.com',
     'accept': 'application/json',
-    'x-tt-token': '005faba7a84f4813ed23b12d5809970bfa05e03e86f0739f57136cdf713cf8882d4182c506438a2ee742d956c4c07bd823d171d6e210a93704326206bffc6295a70fc23fcd8264a30abc4f0096b2c00f077ce1be8a88bdab03b5765dd6b246f597697-1.0.1',
+    'x-tt-token': '005faba7a84f4813ed23b12d5809970bfa05e7c6d6617e95d6a12fb549c5a227bce4fe1f9600e03cd1b28257ddc9963a2d57daaa010c8f22e68d5a25159c5a9c85ef56eb69f34bae60685139c23c21258fd4777e9fe51c00d16b1e99642f6505eb264-1.0.1',
     'content-type': 'application/json; encoding=utf-8',
     'x-ss-cookie': 'excgd=20210223; install_id=3589218274131438; ttreq=1$3a5a53e40a2df4d98d2d83c07db214d3109de2d9; d_ticket=b6a301506d1ee8e4545092c0409c61ae47f75; n_mh=UQpvVN0AqfW7xzagzrxYbBdthPX7OZJPT3sNU3FHi6U; odin_tt=257800b51c5aa6b060e40b2ee3447bc9a3e948eb056bf0d80f7c42e0f4def13984b6af68aa7daa048f05d3ab20bfa352703e2e9d74e55156895639de42e53f96; sessionid=5faba7a84f4813ed23b12d5809970bfa; sessionid_ss=5faba7a84f4813ed23b12d5809970bfa; sid_guard=5faba7a84f4813ed23b12d5809970bfa%7C1613921801%7C5184000%7CThu%2C+22-Apr-2021+15%3A36%3A41+GMT; sid_tt=5faba7a84f4813ed23b12d5809970bfa; uid_tt=c40be95a4d0c0f77bf0b5fd6f6def49f; uid_tt_ss=c40be95a4d0c0f77bf0b5fd6f6def49f; passport_csrf_token=7c29cfe3b0a6c4b85b40e9361f08d72a; passport_csrf_token_default=7c29cfe3b0a6c4b85b40e9361f08d72a',
-    'tt-request-time': '1614075172763',
+    'tt-request-time': '1614236945584',
     'user-agent': 'HotsoonLite 7.6.2 rv:7621 (iPhone; iOS 14.3; zh_CN) Cronet',
     'sdk-version': '1',
     'x-ss-stub': 'D41D8CD98F00B204E9800998ECF8427E',
-    'x-tt-trace-id': '00-ce60991509fbf2b272bbe01112080546-ce60991509fbf2b2-01',
-    'x-khronos': '1614075171',
-    'x-gorgon': '8402a0c70000d2f848cd0a1ccfce27959421ca9fe8c18f0c4794',
+    'x-tt-trace-id': '00-d8050e2a09fbf2b272b3e3a4e1e90546-d8050e2a09fbf2b2-01',
+    'x-khronos': '1614236945',
+    'x-gorgon': '840260f20000a077fcc9c6266c63dbb9884dd2f15a7a36aea60b',
     'Cookie': 'passport_csrf_token=7c29cfe3b0a6c4b85b40e9361f08d72a; passport_csrf_token_default=7c29cfe3b0a6c4b85b40e9361f08d72a; d_ticket=b6a301506d1ee8e4545092c0409c61ae47f75; n_mh=UQpvVN0AqfW7xzagzrxYbBdthPX7OZJPT3sNU3FHi6U; sid_guard=5faba7a84f4813ed23b12d5809970bfa%7C1613921801%7C5184000%7CThu%2C+22-Apr-2021+15%3A36%3A41+GMT; uid_tt=c40be95a4d0c0f77bf0b5fd6f6def49f; uid_tt_ss=c40be95a4d0c0f77bf0b5fd6f6def49f; sid_tt=5faba7a84f4813ed23b12d5809970bfa; sessionid=5faba7a84f4813ed23b12d5809970bfa; sessionid_ss=5faba7a84f4813ed23b12d5809970bfa; odin_tt=257800b51c5aa6b060e40b2ee3447bc9a3e948eb056bf0d80f7c42e0f4def13984b6af68aa7daa048f05d3ab20bfa352703e2e9d74e55156895639de42e53f96; install_id=3589218274131438; ttreq=1$3a5a53e40a2df4d98d2d83c07db214d3109de2d9; excgd=20210223'
 };
 
