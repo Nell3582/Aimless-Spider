f376c04ba549057000ba89750eb99e24cccd23d9
Nell3582 <qianye1203@outlook.com>
2021-02-25 15:44:23 +0800

    change xp workflows

--- iboxpayCOOKIE.js
+++ iboxpayCOOKIE.js
@@ -3,7 +3,10 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
 let refreshtokenVal= `98485ef5a0984bb99fb8527c30076069
-b765d926297a4fc0b18d1df8ec88c8bb`
+b765d926297a4fc0b18d1df8ec88c8bb
+390a5549d9ab4c4085de1f277e72afb1
+a2bcb607757540358175bb6c98d74a1a
+cbd0ad489b1e435782afbbbcaa556b6c`
 
 
 let iboxpaycookie = {
