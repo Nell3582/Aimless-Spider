52a927d8f554bbc556bb162c7fc95f7a411ce505
Nell3582 <qianye1203@outlook.com>
2021-04-12 13:38:34 +0800

    增加账号2

--- iboxpayCOOKIE2.js
+++ iboxpayCOOKIE2.js
@@ -2,9 +2,7 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let refreshtokenVal= `390a5549d9ab4c4085de1f277e72afb1
-a2bcb607757540358175bb6c98d74a1a
-cbd0ad489b1e435782afbbbcaa556b6c`
+let refreshtokenVal= '55ddef98b5194d3db36fa8606f303310'
 
 
 let iboxpaycookie = {
